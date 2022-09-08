1017a8600c167f9ccb37cba3dc3c5c3e20a3fc70
JohnTing <jot.johnting@gmail.com>
2022-09-08 09:15:55 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const ndef = new NDEFReader();
+ndef.scan().then(() => {
+    console.log("Scan started successfully.");
+    ndef.onreadingerror = (event) => {
+        alert("Error! Cannot read data from the NFC tag. Try a different one?");
+        console.log("Error! Cannot read data from the NFC tag. Try a different one?");
+    };
+    ndef.onreading = (event) => {
+        alert("NDEF message read." + event.message);
+        console.log("NDEF message read.");
+    };
+}).catch((error) => {
+    alert(`Error! Scan failed to start: ${error}.`);
+    console.log(`Error! Scan failed to start: ${error}.`);
+});
