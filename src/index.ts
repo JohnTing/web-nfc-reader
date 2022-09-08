bc26727bf8424dee2075ac1eb3d65adbb29ebec4
JohnTing <jot.johnting@gmail.com>
2022-09-08 09:43:03 +0800

    addEventListener

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-const ndef = new NDEFReader();
-ndef.scan().then(() => {
-  console.log("Scan started successfully.");
-  ndef.onreadingerror = (event) => {
-    alert("Error! Cannot read data from the NFC tag. Try a different one?");
-    console.log("Error! Cannot read data from the NFC tag. Try a different one?");
-  };
-  ndef.onreading = (event) => {
-    alert("NDEF message read." + event.message);
-    console.log("NDEF message read.");
-  };
-}).catch((error) => {
-  alert(`Error! Scan failed to start: ${error}.`);
-  console.log(`Error! Scan failed to start: ${error}.`);
+
+const startButton = window.document.getElementById("start") as HTMLButtonElement;
+const logText = window.document.getElementById("log") as HTMLTextAreaElement;
+
+startButton.addEventListener("click", () => {
+  const ndef = new NDEFReader();
+  ndef.scan().then(() => {
+    log("Scan started successfully.");
+    ndef.onreadingerror = () => {
+      log("Cannot read data from the NFC tag. Try another one?");
+    };
+    ndef.onreading = event => {
+      log("NDEF message read.");
+    };
+  }).catch(error => {
+    log(`Error! Scan failed to start: ${error}.`);
+  });
+
 });
+
+
+function log(message: string) {
+  logText.value += message+"\n";
+}
